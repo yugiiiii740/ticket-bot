66f8800d4d75b97114c30d1cf017b1ae59cc8940
yugiiiii740 <yugi3436@gmail.com>
2025-08-07 13:46:49 +0300

    Update createTicket.js

--- utils/createTicket.js
+++ utils/createTicket.js
@@ -1,1 +1,46 @@
+const { PermissionFlagsBits } = require('discord.js');
+const config = require('../config.json');
+
+module.exports = async (interaction) => {
+    const guild = interaction.guild;
+    const member = interaction.member;
+    const selected = interaction.values[0];
+
+    const channelName = `ticket-${member.user.username.toLowerCase()}`;
+
+    const existing = guild.channels.cache.find(ch =>
+        ch.name === channelName &&
+        ch.type === 0
+    );
+
+    if (existing) {
+        return interaction.reply({ content: 'Zaten açık bir ticketin var!', ephemeral: true });
+    }
+
+    const channel = await guild.channels.create({
+        name: channelName,
+        type: 0,
+        parent: config.categoryId,
+        permissionOverwrites: [
+            {
+                id: guild.id,
+                deny: [PermissionFlagsBits.ViewChannel],
+            },
+            {
+                id: member.id,
+                allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages],
+            },
+            {
+                id: config.roles.staff,
+                allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages],
+            }
+        ]
+    });
+
+    await channel.send({
+        content: `🎫 Merhaba ${member}, destek talebin "${selected}" kategorisinde oluşturuldu. Yetkililer kısa sürede seninle ilgilenecektir.`
+    });
+
+    await interaction.reply({ content: `✅ Ticket oluşturuldu: ${channel}`, ephemeral: true });
+};
 
