7baa52b481d255fe45f9a1b227ee49a69acb5e9d
yugiiiii740 <yugi3436@gmail.com>
2025-08-07 13:46:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const { Client, GatewayIntentBits, Collection, Events } = require('discord.js');
+const fs = require('fs');
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+});
+
+client.commands = new Collection();
+
+// Komutları yükle
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// Eventleri yükle
+const eventFiles = fs.readdirSync('./events');
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.login(config.token);
 
