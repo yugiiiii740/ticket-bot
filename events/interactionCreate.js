367df62f807194d4bf052fa451a339001f4d02b0
yugiiiii740 <yugi3436@gmail.com>
2025-08-07 13:46:34 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,16 @@
+const createTicket = require('../utils/createTicket');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+        if (interaction.isChatInputCommand()) {
+            const command = client.commands.get(interaction.commandName);
+            if (command) await command.execute(interaction, client);
+        }
+
+        if (interaction.isStringSelectMenu() && interaction.customId === 'ticket_select') {
+            await createTicket(interaction);
+        }
+    }
+};
 
